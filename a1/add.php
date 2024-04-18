d729d95436e37152ff74850b0f9a5461a4a9b4a1
s3899226 <s3899226@student.rmit.edu.au>
2024-04-18 21:44:29 +1000

    add page can now add to database

--- a1/add.php
+++ a1/add.php
@@ -1,4 +1,47 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        include('db_connect.inc');
+        $hikename = $_POST["hikename"];
+        $description = $_POST["description"];
+        $image = $_POST["image"];
+        $caption = $_POST["caption"];
+        $distance = $_POST["distance"];
+        $location = $_POST["location"];
+        $level = $_POST["level"];
+
+        
+        try {
+            require_once "db_connect.inc";
+
+            $query = "INSERT INTO hikes (hikename, description, image, caption, distance, location, level) VALUES 
+            (?, ?, ?, ?, ?, ?, ?);";
+
+            $stmt = $pdo->prepare($query);
+
+            $stmt->execute([$hikename, $description, $image, $caption, $distance, $location, $level]);
+
+
+            // if ($stmt->affected_rows > 0) {
+            //     echo "A new record has been created";
+            // }
+
+            $pdo = null;
+            $stmt = null;
+            header("Location: ./add.php");
+            die();
+        } catch (PDOException $e) {
+            die("Query failed: " . $e->getMessage());
+        }
+    }
+    else {
+        // header("Location: ./add.php");
+        // die();
+    }
+?>
+
 <?php include './includes/header.inc'; ?>
+
+
 
 
 <body>
@@ -17,17 +60,21 @@
             </div>
 
             <div class="add-box">
-                <form>
-                    <label class="required" for="hname">Hike Name </label><br>
-                    <input type="text" id="hname" name="hname" required placeholder="Name of hike"><br>
+                <form action="" method="POST">
+
+
+                    <label class="required" for="hikename">Hike Name </label><br>
+                    <input type="text" id="hikename" name="hikename" required placeholder="Name of hike"><br>
+
+
                     <label class="required" for="description">Description </label><br>
                     <textarea id="description" name="description" required placeholder="Describe the hike"></textarea><br>
 
-                    <label class="required" for="select-image">Select an image </label><br>
-                    <input type="file" id="select-image" name="select-image" required><br>
+                    <label class="required" for="image">Select an image </label><br>
+                    <input type="file" id="image" name="image" required><br>
 
-                    <label class="required" for="image-caption">Image Caption </label><br>
-                    <input type="text" id="image-caption" name="image-caption" required><br>
+                    <label class="required" for="caption">Image Caption </label><br>
+                    <input type="text" id="caption" name="caption" required><br>
 
                     <label class="required" for="distance">Distance </label><br>
                     <input type="text" id="distance" name="distance" required placeholder="Distance in km"><br>
