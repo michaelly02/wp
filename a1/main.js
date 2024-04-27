baa0912c4b4f5057848485eb21bb4771b7d1b4d5
s3899226 <s3899226@student.rmit.edu.au>
2024-04-27 20:50:26 +1000

    replaced a1

--- a1/main.js
+++ a1/main.js
@@ -1,11 +1,6 @@
 function redirectToPage() {
     var selectedPage = document.getElementById("redirectSelect").value;
-
-    console.log("Selected page:", selectedPage);
-    // console.log("Hike ID:", hikeId);
     if (selectedPage) {
       window.location.href = selectedPage;
-
-      
     }
   }
