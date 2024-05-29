d810081fde3dae7c15b2795dc65344faeb4f7234
s3899226 <s3899226@student.rmit.edu.au>
2024-05-29 20:18:37 +1000

    finished off everything

--- a3/search.php
+++ a3/search.php
@@ -5,18 +5,19 @@
 
     <main class="home-container">
 
-    <form action="search.php" method="GET" class="index-form">
-                <input class="index-form-item"  type="text" id="search" name="search" placeholder="I am looking for">
-                
-                <select class="index-form-item" id="level" name="level">
-                    <option value="">Select level of adventure</option>
-                    <option value="easy">Easy</option>
-                    <option value="moderate">Moderate</option>
-                    <option value="hard">Hard</option>
-                </select>
-                
-                <button type="submit" class="index-form-button" >Search</button>
-            </form>
+        <form action="search.php" method="GET" class="index-form">
+            <input class="index-form-item" type="text" id="search" name="search" placeholder="I am looking for">
+
+            <select class="index-form-item" id="level" name="level">
+                <option value="">Select level of adventure</option>
+                <option value="easy">Easy</option>
+                <option value="moderate">Moderate</option>
+                <option value="hard">Hard</option>
+            </select>
+
+            <button type="submit" class="index-form-button">Search</button>
+        </form>
+
         <?php
         include("db_connect.inc");
 
@@ -27,21 +28,28 @@
         // Construct the SQL query based on the search query and difficulty
         $sql = "SELECT * FROM hikes WHERE 1=1"; // Initial query
         if (!empty($searchQuery)) {
-            $sql .= " AND hikename LIKE '%$searchQuery%'"; // Add search filter
+            $sql .= " AND (hikename LIKE :searchQuery OR description LIKE :searchQuery)"; // Add search filter
         }
         if (!empty($level)) {
-            $sql .= " AND level = '$level'"; // Add level filter
+            $sql .= " AND level = :level"; // Add level filter
         }
 
-        // Execute the query
-        $stmt = $pdo->query($sql);
+        // Prepare and execute the query
+        $stmt = $pdo->prepare($sql);
+        if (!empty($searchQuery)) {
+            $stmt->bindValue(':searchQuery', '%' . $searchQuery . '%', PDO::PARAM_STR);
+        }
+        if (!empty($level)) {
+            $stmt->bindValue(':level', $level, PDO::PARAM_STR);
+        }
+        $stmt->execute();
 
         // Fetch hike data and generate gallery HTML
         echo '<div class="search-wrapper">';
         while ($hike = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo '<a class="box" href="details.php?id=' . $hike['hikeid'] . '">';
-            echo '<img src="' . $hike['image'] . '" alt="' . $hike['hikename'] . '">';
-            echo '<div class="caption">' . $hike['hikename'] . '</div>';
+            echo '<a class="box" href="details.php?id=' . htmlspecialchars($hike['hikeid']) . '">';
+            echo '<img src="' . htmlspecialchars($hike['image']) . '" alt="' . htmlspecialchars($hike['hikename']) . '">';
+            echo '<div class="caption">' . htmlspecialchars($hike['hikename']) . '</div>';
             echo '</a>';
         }
         echo '</div>';
